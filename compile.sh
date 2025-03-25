6ad7da09f218b2c41f2e76e60c6c8976fefd8b0f
Luan Willig Silveira <luan.w.silveira@gmail.com>
2025-03-24 22:58:34 -0300

    Update compile.sh

--- compile.sh
+++ compile.sh
@@ -4,8 +4,8 @@ chmod u+w out
 chmod -R u+w out
 touch out/main.log
 chmod u+w out/main.log
-pdflatex -shell-escape -output-directory=out main.tex
+pdflatex -shell-escape -interaction=nonstopmode -output-directory=out main.tex
 bibtex out/main
-pdflatex -shell-escape -output-directory=out main.tex
-pdflatex -shell-escape -output-directory=out main.tex
+pdflatex -shell-escape -interaction=nonstopmode -output-directory=out main.tex
+pdflatex -shell-escape -interaction=nonstopmode -output-directory=out main.tex
 cd ../
