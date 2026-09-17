3d2ce5e1a61589c0f86eec3f2975722df0555820
Luan Willig Silveira <luan.w.silveira@gmail.com>
2026-09-17 00:56:31 -0300

    apresentação em ptbr

--- presentation/build/build.js
+++ presentation/build/build.js
@@ -3,6 +3,7 @@
 // Uso: node presentation/build/build.js [saida.pptx]
 const fs = require("fs");
 const path = require("path");
+const JSZip = require("jszip");
 const { pres } = require("./comum");
 
 const SLIDES = path.join(__dirname, "slides");
@@ -12,6 +13,20 @@ fs.readdirSync(SLIDES)
   .sort()
   .forEach(nome => require(path.join(SLIDES, nome))());
 
+// O pptxgenjs não expõe opção de idioma para addNotes() e grava lang="en-US"
+// fixo no XML das notas do orador. Corrige isso após gerar o arquivo, para o
+// corretor ortográfico do PowerPoint tratar a fala também como português.
+async function corrigirIdiomaDasNotas(caminhoPptx) {
+  const zip = await JSZip.loadAsync(fs.readFileSync(caminhoPptx));
+  const notas = zip.folder("ppt/notesSlides").file(/\.xml$/);
+  for (const arquivo of notas) {
+    const xml = await arquivo.async("string");
+    zip.file(arquivo.name, xml.replace(/lang="en-US"/g, 'lang="pt-BR"'));
+  }
+  fs.writeFileSync(caminhoPptx, await zip.generateAsync({ type: "nodebuffer" }));
+}
+
 const saida = path.resolve(process.argv[2] || path.join(__dirname, "..", "defesa.pptx"));
 pres.writeFile({ fileName: saida })
+  .then(f => corrigirIdiomaDasNotas(f).then(() => f))
   .then(f => console.log("Gerado:", f));
